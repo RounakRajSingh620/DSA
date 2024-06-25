ba51ed2ec542a8c457a48627ab6bcbf5e5170611
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2024-06-25 20:35:19 +0530

    BinaryRecurrsion

--- Binaryrecursion.cpp
+++ Binaryrecursion.cpp
@@ -1,14 +1,28 @@
 #include <iostream>
 using namespace std;
 
+void print(int arr[], int s, int e)
+{
+    for (int i = s; i <= e; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
 bool binarySearch(int *arr, int s, int e, int key)
 {
+    print(arr, s, e);
     // base case
     if (s > e)
     {
-        return -1;
+        return false;
     }
-    int mid = (s + e) / 2;
+    int mid = s + (e - s) / 2;
+    if (arr[mid] == key)
+    {
+        return true;
+    }
     if (arr[mid] < key)
     {
         return binarySearch(arr, mid + 1, e, key);
@@ -18,11 +32,14 @@ bool binarySearch(int *arr, int s, int e, int key)
         return binarySearch(arr, s, mid - 1, key);
     }
 }
+
 int main()
 {
     int arr[6] = {2, 4, 6, 3, 5, 7};
     int size = 6;
-    int key = 5;
+    int key = 3;
+
+    cout << "Present or Not  " << binarySearch(arr, 0, 5, key) << endl;
 
     return 0;
 }
